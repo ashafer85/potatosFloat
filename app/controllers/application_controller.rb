3abc0dde910cbd1c235852c369d82e6b146de464
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-22 10:36:57 -0400

    Creating BackEnd Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?, :login, :logout
+
+  def current_user
+    User.find_by(session_token: session[:session_token])
+  end
+
+  # def ensure_logged_in
+  #   redirect unless logged_in?
+  #
+  # end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_token!
+  end
+
+  def logout()
+    session[:session_token] = nil
+  end
+
+
 end
