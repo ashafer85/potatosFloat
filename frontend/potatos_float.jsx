dc122f84fe2f8346b7c86869c48c99cb371123d0
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-22 13:31:59 -0400

    Create rootReducer

--- frontend/potatos_float.jsx
+++ frontend/potatos_float.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import configureStore from './store/store';
 // import Root from './components/root';
 import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const store
+  // const store = configureStore();
   // window.getState = store.getState;
   window.signup = signup;
   window.login = login;
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,25 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+import { merge } from 'lodash';
+
+const _defaultSession = {
+  currentUser: null,
+  errors: [],
+};
+
+export const sessionReducer = ( state = _defaultSession, action) => {
+
+  Object.freeze(state);
+
+  switch(action.type) {
+
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.user;
+      return merge({}, state, { currentUser });
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, state, { errors })
+    default:
+      return state;
+  }
+
+}
