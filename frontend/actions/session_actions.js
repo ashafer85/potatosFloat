cf38e23b45e63eceeec3a11050741cc860ff127d
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-22 12:23:16 -0400

    Create session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = (currentUser) => {
+  return ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser,
+  });
+};
+
+export const receiveErrors = (errors) => {
+  return ({
+    type: RECEIVE_ERRORS,
+    errors,
+  })
+};
+
+export const signup = (user) => {
+  return APIUtil.signup(user)
+    .then((res) => {
+      return dispatch(receiveCurrentUser())
+    });
+};
+
+export const login = (user) => {
+  return APIUtil.login(user)
+    .then((res) => {
+      return dispatch(receiveCurrentUser())
+    });
+};
+
+export const logout = () => {
+  return APIUtil.logout()
+    .then((res) => {
+      return dispatch(receiveCurrentUser(null))
+    });
+};
