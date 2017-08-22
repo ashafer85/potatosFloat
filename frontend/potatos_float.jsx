da429dcb824107b1b0ab8e76cb1b4bad06fb1b20
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-22 12:07:39 -0400

    Create ajax calls for handing frontend auth in session_api_util

--- frontend/potatos_float.jsx
+++ frontend/potatos_float.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import Root from './components/root';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // const store
+  // window.getState = store.getState;
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
 
   const root = document.getElementById('root');
   ReactDOM.render(<h1> Welcome to potatosFloat </h1>, root);
-  
+
 });
