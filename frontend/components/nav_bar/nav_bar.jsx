d283aa23755e7e00c629e97a10f23623f585bdcf
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-22 18:32:37 -0400

    Style the simple/static version of the NavBar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -6,14 +6,21 @@ class NavBar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
 
   render () {
     return(
-      <ul>
-        <li>Item1</li>
-        <li>Item2</li>
-      </ul>
+      <div className='navBar'>
+        <div className='logo'>potatosfloat</div>
+        <ul className='auth'>
+          <li className='join'>Join</li>
+          <li className='login'>Log In</li>
+        </ul>
+      </div>
     );
   }
 
