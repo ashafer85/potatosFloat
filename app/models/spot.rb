f5fa8b04a01b85c7e924d8f61b4bb1b61362a864
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-28 04:28:28 -0400

    Create spot model

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,9 @@
 class Spot < ApplicationRecord
+  validates :title, :description, :capacity, presence: true
+  validates :city, :lat, :lng, presence: true
+  validates :sleep_arrangement, :roommate_situation, :public_trans, presence: true
+  validates :kid, :pet, :smoking, :wheelchair, presence: true
+
+
+
 end
