20bdac8b0ad850fad6bcb8af2054fe7acb6129fa
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-28 11:54:43 -0400

    Debug Spot and User validation bugs

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,9 +1,19 @@
 class Spot < ApplicationRecord
   validates :title, :description, :capacity, presence: true
   validates :city, :lat, :lng, presence: true
-  validates :sleep_arrangement, :roommate_situation, :public_trans, presence: true
-  validates :kid, :pet, :smoking, :wheelchair, presence: true
 
+  # validates :sleep_arrangement, presence: true
+  # , default: 'One Couch'
+  # validates :roommate_situation, presence: true
+  # , default: 'none'
+  # validates :public_trans, presence: true
+  # , default: 'its New York...'
+  validates :kid, :pet, :smoking, :wheelchair, default: false, inclusion: [true, false]
+  validates :host_id, presence: true
 
+  belongs_to :host,
+    class_name: :User,
+    primary_key: :id,
+    foreign_key: :host_id
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
+  has_many :spots,
+    class_name: :Spot,
+    primary_key: :id,
+    foreign_key: :host_id
+
   after_initialize :ensure_session_token
   attr_reader :password
 
