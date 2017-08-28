a8ad67825c28ec73a2b15d0f5a22d445c9ba0f6f
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-28 12:04:10 -0400

    Update  spotsReducer

--- frontend/reducers/spots_reducer.js
+++ frontend/reducers/spots_reducer.js
@@ -1,15 +1,20 @@
-
-import { RECEIVE_SPOTS, RECEIVE_SPOT } from '../actions/spots_actions';
+import { merge } from 'lodash';
+import {
+  RECEIVE_SPOTS,
+  RECEIVE_SPOT
+} from '../actions/spots_actions';
 
 export const spotsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch(action.type) {
     case RECEIVE_SPOTS:
-      const spots = action.spots;
-      return merge( {}, state, { spots } );
+      // const spots = action.payload;
+      return merge( {}, state, action.spots );
     case RECEIVE_SPOT:
-      const spot = action.spots;
+      const spot = action.payload;
+      // spot.review_ids = action.payload.reviews.map(review => review.id);
+      return merge({}, state, { [spot.id]: spot });
     default:
       return state;
   };
