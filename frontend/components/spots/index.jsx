31d5d07735e3c0df34221674e7ebd3ec5b739779
Andrew Shafer <andrewshafer15@gmail.com>
2017-08-28 18:03:32 -0400

    End of W9D1 - Formatting Spots

--- frontend/components/spots/index.jsx
+++ frontend/components/spots/index.jsx
@@ -22,7 +22,36 @@ class SpotsIndex extends React.Component {
     return(
       <div className='spotsIndexContainer'>
         <span className='spotsIndexHeader'>
-          <h2> SPOTS: </h2>
+
+          <h2> Spots in New York: </h2>
+
+          <form className='spotSearchForm'>
+
+            <div className='boundaryDates'>
+              <div className='date'>
+                <label> ARRIVE </label>
+                <input type='dateTime' className='spotDate arrival' placeholder='yy/mm/dd'/>
+              </div>
+              <div className='date'>
+                <label> DEPART </label>
+                <input type='dateTime' className='spotDate departure' placeholder='yy/mm/dd'/>
+              </div>
+            </div>
+
+            <label className='travelerCount'> # OF TRAVELERS
+              <input type='integer' className='' placeholder='1'/>
+            </label>
+
+
+            <div className='moreFilters'>
+              <main className='linkToMoreFilters'>
+                <i className="fa fa-filter" aria-hidden="true"></i>
+                <tag> More Filters </tag>
+              </main>
+            </div>
+
+          </form>
+
         </span>
 
         <ul className='spotsIndex'>
