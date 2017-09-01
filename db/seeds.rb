065f34ba8bda80bd37c18eda3e4a938431963579
Andrew Shafer <andrewshafer15@gmail.com>
2017-09-01 13:52:32 -0400

    final heroku push

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,6 +33,90 @@ user4 = User.create!(
   'gender' => 'Female',
   'age' => 29
 )
+user5 = User.create!(
+  'username' => 'George',
+  'password' => 'gfgfgf',
+  'gender' => 'Male',
+  'age' => 24
+)
+user6 = User.create!(
+  'username' => 'Joe',
+  'password' => 'sissis',
+  'gender' => 'Female',
+  'age' => 27
+)
+user7 = User.create!(
+  'username' => 'John',
+  'password' => 'friend',
+  'gender' => 'Male',
+  'age' => 24
+)
+user8 = User.create!(
+  'username' => 'Roger',
+  'password' => 'password',
+  'gender' => 'Female',
+  'age' => 29
+)
+user9 = User.create!(
+  'username' => 'Sarah',
+  'password' => 'gfgfgf',
+  'gender' => 'Female',
+  'age' => 24
+)
+user10 = User.create!(
+  'username' => 'Tyler',
+  'password' => 'gfgfgf',
+  'gender' => 'Female',
+  'age' => 24
+)
+user11 = User.create!(
+  'username' => 'Megan',
+  'password' => 'gfgfgf',
+  'gender' => 'Female',
+  'age' => 24
+)
+user12 = User.create!(
+  'username' => 'Natalie',
+  'password' => 'sissis',
+  'gender' => 'Female',
+  'age' => 27
+)
+user13 = User.create!(
+  'username' => 'Grant',
+  'password' => 'friend',
+  'gender' => 'Male',
+  'age' => 24
+)
+user14 = User.create!(
+  'username' => 'Peyton',
+  'password' => 'password',
+  'gender' => 'Female',
+  'age' => 29
+)
+user15 = User.create!(
+  'username' => 'TJ',
+  'password' => 'gfgfgf',
+  'gender' => 'Male',
+  'age' => 24
+)
+user16 = User.create!(
+  'username' => 'Blake',
+  'password' => 'sissis',
+  'gender' => 'Female',
+  'age' => 27
+)
+user17 = User.create!(
+  'username' => 'Eric',
+  'password' => 'friend',
+  'gender' => 'Male',
+  'age' => 24
+)
+user18 = User.create!(
+  'username' => 'Simon',
+  'password' => 'password',
+  'gender' => 'Female',
+  'age' => 29
+)
 
 Spot.destroy_all
 Spot.create!(
@@ -84,10 +168,86 @@ Spot.create!(
   'city' => 'New York City',
   'lat' => 40.745200,
   'lng' => -73.994082,
+  'sleep_option' => 'Public Room',
+  'host_id' => user4.id,
+)
+Spot.create!(
+  'title' => 'User 5 Spot',
+  'description' => 'Near the water',
+  'capacity' => 1,
+  'city' => 'New York City',
+  'sleep_option' => 'Private Room',
+  'lat' => 40.745200,
+  'lng' => -73.994082,
   # 'sleep_arrangement' => 'Couch in the former corner office',
   # 'roommate_situation' => 'Multiple roommates come a go quietly',
   # 'public_trans' => 'Two blocks from the red 1 2 and grean 5',
-  'host_id' => user4.id,
+  'host_id' => user5.id,
+)
+Spot.create!(
+  'title' => 'User 6 Spot',
+  'description' => 'Union Square',
+  'capacity' => 2,
+  'city' => 'New York City',
+  'lat' => 40.745200,
+  'lng' => -73.994082,
+  'sleep_option' => 'Shared Bed',
+  # 'sleep_arrangement' => 'Couch in the former corner office',
+  # 'roommate_situation' => 'Multiple roommates come a go quietly',
+  # 'public_trans' => 'Two blocks from the red 1 2 and grean 5',
+  'host_id' => user6.id,
+)
+Spot.create!(
+  'title' => 'User 7 Spot',
+  'description' => 'FiDi studio',
+  'capacity' => 1,
+  'city' => 'New York City',
+  'lat' => 40.745200,
+  'lng' => -73.994082,
+  'sleep_option' => 'Shared Bed',
+  # 'sleep_arrangement' => 'Couch in the former corner office',
+  # 'roommate_situation' => 'Multiple roommates come a go quietly',
+  # 'public_trans' => 'Two blocks from the red 1 2 and grean 5',
+  'host_id' => user7.id,
+)
+Spot.create!(
+  'title' => 'User 8 Spot',
+  'description' => 'Central Park West',
+  'capacity' => 2,
+  'city' => 'New York City',
+  'lat' => 40.745200,
+  'lng' => -73.994082,
+  'sleep_option' => 'Private Room',
+  # 'sleep_arrangement' => 'Couch in the former corner office',
+  # 'roommate_situation' => 'Multiple roommates come a go quietly',
+  # 'public_trans' => 'Two blocks from the red 1 2 and grean 5',
+  'host_id' => user8.id,
+)
+Spot.create!(
+  'title' => 'User 9 Spot',
+  'description' => 'Columbia Campus',
+  'capacity' => 1,
+  'city' => 'New York City',
+  'lat' => 40.745200,
+  'lng' => -73.994082,
+  'sleep_option' => 'Public Room',
+  # 'sleep_arrangement' => 'Couch in the former corner office',
+  # 'roommate_situation' => 'Multiple roommates come a go quietly',
+  # 'public_trans' => 'Two blocks from the red 1 2 and grean 5',
+  'host_id' => user9.id,
+)
+Spot.create!(
+  'title' => 'User 10 Spot',
+  'description' => 'Harlem',
+  'capacity' => 1,
+  'city' => 'New York City',
+  'lat' => 40.745200,
+  'lng' => -73.994082,
+  'sleep_option' => 'Shared Room',
+  # 'sleep_arrangement' => 'Couch in the former corner office',
+  # 'roommate_situation' => 'Multiple roommates come a go quietly',
+  # 'public_trans' => 'Two blocks from the red 1 2 and grean 5',
+  'host_id' => user10.id,
 )
 
 
